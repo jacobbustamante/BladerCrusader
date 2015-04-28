0701cd2ccc97ceb3383be6e859f1b7c4efe549fd
jacobbustamante <jacobandrewbustamante@gmail.com>
2015-04-28 16:09:39 -0700

    Built base Game and Menu scenes

---
+++ Assets/Scripts/MenuPlayButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuPlayButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Button>().onClick.AddListener(this.OnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick() {
+		Application.LoadLevel("Game");
+	}
+}
