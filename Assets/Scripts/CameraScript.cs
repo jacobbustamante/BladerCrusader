20f48ddf666198dbb03a12ecc56f06e6228d2996
jacobbustamante <jacobandrewbustamante@gmail.com>
2015-05-08 00:31:04 -0700

    Initial implementation of the Map, GameManager, and camera. All placed in the Game scene.

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+	public float cameraHeight = -10;
+	
+	private GameManagerScript gameManager;
+	private GameObject playerInstance;
+	private Camera camera;
+
+	private int mapHeight, mapWidth;
+	private float minX, minY, maxX, maxY;
+	private int prevScreenWidth = -1, prevScreenHeight = -1;
+	
+	void Awake() {
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+		camera = this.GetComponent<Camera>();
+	}
+
+	void Start () {
+		mapWidth = gameManager.mapWidth;
+		mapHeight = gameManager.mapHeight;
+		playerInstance = gameManager.GetPlayerInstance();
+	}
+
+	void Update () {
+		if(Screen.width != prevScreenWidth || Screen.height != prevScreenHeight)
+			UpdateBorders();
+		
+		this.transform.position = new Vector3(
+			Mathf.Clamp(playerInstance.transform.position.x, minX, maxX),
+			Mathf.Clamp(playerInstance.transform.position.y, minY, maxY),
+			cameraHeight);
+	}
+
+	void UpdateBorders() {
+		float tileOffset = -0.5f;
+
+		float camViewY = camera.orthographicSize;
+		float camViewX = camViewY * (float)Screen.width / (float)Screen.height;
+
+		minX = camViewX + tileOffset;
+		minY = camViewY + tileOffset;
+		maxX = mapWidth - camViewX + tileOffset;
+		maxY = mapHeight - camViewY + tileOffset;
+
+		prevScreenWidth = Screen.width;
+		prevScreenHeight = Screen.height;
+	}
+
+}
---
+++ Assets/Scripts/Managers/GameManagerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public GameObject heroPrefab;
+	public int mapWidth, mapHeight;
+
+	private GameObject playerInstance;
+
+	void Awake() {
+		PlaceHero();
+	}
+
+	// Use this for initialization
+	void Start () {
+		playerInstance.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void PlaceHero() {
+		GameObject hero = Object.Instantiate(heroPrefab);
+		hero.transform.position = new Vector3(mapWidth / 2.0f, mapHeight / 2.0f, hero.transform.position.z);
+		hero.SetActive(false);
+
+		playerInstance = hero;
+	}
+
+	public GameObject GetPlayerInstance() {
+		return playerInstance;
+	}
+}
