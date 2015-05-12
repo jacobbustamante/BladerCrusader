b4e862a57c928057e78575c3076e7d2414d62b0c
Nathan Farnum <njfarnum@Nathans-MacBook-Pro.local>
2015-05-11 17:14:31 -0700

    More levelManager stuff and spawnpoints

---
+++ Assets/EnemyPool.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPool : MonoBehaviour {
+
+	public GameObject Org;
+	public GameObject HighOrg;
+	public GameObject BezerkerOrg;
+	public GameObject Specty;
+	public GameObject StakeHolder;
+	public GameObject Wizlock;
+	public GameObject Snell;
+
+	public static EnemyPool enemyPool;
+
+	void Awake() {
+		if (enemyPool == null) {
+			DontDestroyOnLoad(this.gameObject);
+			enemyPool = this;
+		} else if (enemyPool != this) {
+			Destroy (this.gameObject);
+		} 
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/EnemySpawnPointScript.cs
+++ Assets/EnemySpawnPointScript.cs
@@ -1,22 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnemySpawnPointScript : MonoBehaviour {
 
 	public int enemyCountToSpawn;
-	public GameObject enemyType;
-	public int spawnDelay;
+	public List<GameObject> enemyTypes;
+	public float spawnDelay;
 	public bool done;
 	public int enemiesDefeated;
+	int level;
 
 	// Use this for initialization
 	void Start () {
-	
+		level = GameManagerScript.gameManager.level;
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void SetSpawnPointParams() {
+
+		if (level < 3) {
+			spawnDelay = 5.0f;
+			enemyTypes.Add(EnemyPool.enemyPool.Org);
+			enemyTypes.Add(EnemyPool.enemyPool.Specty);
+			enemyTypes.Add(EnemyPool.enemyPool.Snell);
+
+		} else if (level < 6) {
+			spawnDelay = 4.0f;
+
+		} else if (level < 9) {
+			spawnDelay = 3.0f;
+		} else if (level < 12) {
+			spawnDelay = 2.0f;
+		} else if (level < 15) {
+			spawnDelay = 1.0f;
+		} else {
+			spawnDelay = 0.0f;
+		}
 	}
 
 
