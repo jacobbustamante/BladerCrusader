d3f943dadefeedc4ac6a11b09c9180e425cbef90
jacobbustamante <jacobandrewbustamante@gmail.com>
2015-05-14 13:37:03 -0700

    Started implementing Enemy super class and org sub class.

--- Assets/Scripts/Enemies/EnemyOrgScript.cs
+++ Assets/Scripts/Enemies/EnemyOrgScript.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class placeholder : MonoBehaviour {
+public class EnemyOrgScript : EnemyScript {
+
+	void Awake() {
+		base.Awake();
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		base.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		base.Update();
+		MoveDirectlyTowardsTarget();
 	}
 }
---
+++ Assets/Scripts/Enemies/EnemyScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyScript : MonoBehaviour {
+
+	public int level = 1;
+	public int hitPoints = 10;
+	public int baseAttack = 1;
+	public int baseDefense = 1;
+	public int baseSpeed = 1;
+
+	protected EnemySpawnPointScript originSpawnPoint;
+	protected Rigidbody2D rb;
+	protected GameObject target;
+
+	protected virtual void Awake() {
+		rb = this.GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	protected virtual void Start () {
+		UpdateTarget();
+	}
+
+	// Update is called once per frame
+	protected virtual void Update () {
+		UpdateTarget();
+	}
+
+	protected void MoveDirectlyTowardsTarget() {
+		if (target != null) {
+			Vector2 direction = target.transform.position - this.transform.position;
+			rb.velocity = direction.normalized * baseSpeed;
+		}
+	}
+	
+	protected void UpdateTarget() {
+		if (target == null)
+			target = GameManagerScript.gameManager.GetPlayerInstance();
+	}
+
+}
