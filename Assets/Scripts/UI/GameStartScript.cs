6ff1b5f7acca4c0c84ef5045f3c111a6cf437eda
jacobbustamante <jacobandrewbustamante@gmail.com>
2015-06-09 17:57:35 -0700

    Adding a Unity Web Player branch.

--- Assets/Scripts/UI/GameStartScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GameStartScript : MonoBehaviour {
-
-	public void StartGame() {
-		GameManagerScript.gameManager.UnpauseGame();
-	}
-}
--- README.md
+++
@@ -1,8 +0,0 @@
-# BladerCrusader
-CPE-309 Spring 2015 Final Project
-Delaney Giacalone
-Gabriel Hernandez
-Brian Fung
-Nathan Farnum
-Jacob Bustamante
-Mitch Lane
